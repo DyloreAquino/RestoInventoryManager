5dd896e8ce32b468ebd062523294c4e7f03c4909
DyloreAquino <lutherjerold@gmail.com>
2026-01-28 10:56:27 +0800

    Create ts files for all objects. Also includes transaction commits and data validation.

---
+++ RestoInventoryManager/src/db/items.ts
@@ -0,0 +1,74 @@
+import {getDB} from "./db";
+
+// Functions to manage items in the inventory
+export async function createItem(
+    name: string,
+    quantity: number
+) {
+    const db = await getDB();
+    await db.runAsync(
+        "INSERT INTO items (name, quantity) VALUES (?, ?);",
+        [name, quantity]
+    );
+}
+
+// Retrieves all items from the inventory
+export async function getItems() {
+    const db = await getDB();
+    return await db.getAllAsync(
+        "SELECT * FROM items;"
+    );
+}
+
+
+// Increases the quantity of a specific item
+export async function increaseItemQuantity(
+    id: number,
+    quantity: number
+) {
+    const db = await getDB();
+    await db.runAsync(
+        "UPDATE items SET quantity = quantity + ? WHERE id = ?;",
+        [quantity, id]
+    );
+}
+
+// Decreases the quantity of a specific item
+export async function decreaseItemQuantity(
+    itemID: number,
+    quantity: number
+) {
+    const db = await getDB();
+
+    const item = await db.getAllAsync(
+        "SELECT quantity FROM items WHERE id = ?;",
+        [itemID]
+    ) as { quantity: number }[];
+
+    if (!item || item.length === 0) {
+        throw new Error(`Item with ID ${itemID} not found`);
+    }
+
+    const currentQuantity = item[0].quantity;
+
+    if (currentQuantity < quantity) {
+        throw new Error(
+            `Cannot decrease item ID ${itemID} by ${quantity}. Only ${currentQuantity} in stock.`
+        );
+    }
+
+    await db.runAsync(
+        "UPDATE items SET quantity = quantity - ? WHERE id = ?;",
+        [quantity, itemID]
+    );
+}
+
+
+// Deletes an item from the inventory
+export async function deleteItem(id: number) {
+    const db = await getDB();
+    await db.runAsync(
+        "DELETE FROM items WHERE id = ?;",
+        [id]
+    );
+}
---
+++ RestoInventoryManager/src/db/orders.ts
@@ -0,0 +1,72 @@
+import {getDB} from "./db";
+import { decreaseItemQuantity } from "./items";
+
+// Types
+export type OrderDish = { dishID: number; quantity: number };
+export type DishItemStock = { itemID: number; quantity: number };
+
+export async function createOrder(
+    orderDishes: OrderDish[]
+) {
+    const db = await getDB();
+    const orderDate = new Date().toISOString().split('T')[0];
+    await db.execAsync("BEGIN");
+    try {
+        const order = await db.runAsync(
+            "INSERT INTO orders (orderDate) VALUES (?);",
+            [orderDate]
+        );
+        const orderID = order.lastInsertRowId;
+
+        for (const order_dishes of orderDishes) {
+            await db.runAsync(
+                "INSERT INTO order_dish (orderID, dishID, quantity) VALUES (?, ?, ?);",
+                [orderID, order_dishes.dishID, order_dishes.quantity]
+            );
+
+            const dish_items= await db.getAllAsync(
+                `SELECT itemID, quantity FROM dish_item WHERE dishID = ?;`,
+                [order_dishes.dishID]
+            ) as DishItemStock[];
+
+            for (const dish_item of dish_items) {
+                const totalRequired = dish_item.quantity * order_dishes.quantity;
+                await decreaseItemQuantity(dish_item.itemID, totalRequired);
+            }
+        }
+
+        await db.execAsync("COMMIT");
+        return orderID;
+    } catch (err) {
+        await db.execAsync("ROLLBACK");
+        throw err;
+    }
+}
+
+export async function getOrderDishes(
+    orderID: number
+) {
+    const db = await getDB();
+    return await db.getAllAsync(
+       `SELECT dishes.id, dishes.dishName, order_dish.quantity
+        FROM order_dish
+        JOIN dishes ON order_dish.dishID = dishes.id
+        WHERE order_dish.orderID = ?;`,
+        [orderID]
+    );
+}
+
+export async function getOrders() {
+    const db = await getDB();
+    return await db.getAllAsync(
+        "SELECT * FROM orders;"
+    );
+}
+
+export async function deleteOrder(id: number) {
+    const db = await getDB();
+    await db.runAsync(
+        "DELETE FROM orders WHERE id = ?;",
+        [id]
+    );
+}
