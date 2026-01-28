5e1d19f3b4fb460e457a9c250f2eea9d430a82c1
DyloreAquino <lutherjerold@gmail.com>
2026-01-28 15:48:50 +0800

    Modified list layouts.

--- RestoInventoryManager/app/(tabs)/_layout.tsx
+++ RestoInventoryManager/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import InitScreen from "../init"; // wrap the tabs with DB init
 export default function Layout() {
   return (
     <InitScreen>
-      <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs screenOptions={{ headerShown: true }}>
         <Tabs.Screen name="index" options={{ title: "Home" }} />
         <Tabs.Screen name="orders" options={{ title: "Orders" }} />
         <Tabs.Screen name="deliveries" options={{ title: "Deliveries" }} />
